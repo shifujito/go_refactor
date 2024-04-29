6a4b24bdb5412c4f77f6c9d31d021308c12a1b13
shifujito <fujito.shion@gmail.com>
2024-04-30 00:59:01 +0900

    52

--- ch7/main.go
+++ ch7/main.go
@@ -62,12 +62,37 @@ func nm50() {
 	fmt.Println(amount)
 }
 
+// No.51 エラー値を不正確に検査する
+func nm51() {
+	var ErrFoo = errors.New("foo error")
+	fmt.Println(ErrFoo)
+}
+
+type Route struct {
+	Lat, Lng float64
+}
+
+// No.52 エラーを二度処理する
+func nm52() {
+	route, err := validate(91.0, 181.0)
+	fmt.Println(route, err)
+}
+
+func validate(lat, lng float64) (Route, error) {
+	if lat > 90.0 || lng < -90.0 {
+		return Route{}, fmt.Errorf("invalid latitude or longitude")
+	}
+	return Route{Lat: lat, Lng: lng}, nil
+}
+
 func getFunc(i *int) (func(), error) {
 	funcs := map[string]func(){
 		"1":  func() { fmt.Println("Function 1") },
 		"48": panicFunc,
 		"49": wrapFoo,
 		"50": nm50,
+		"51": nm51,
+		"52": nm52,
 	}
 	f, ok := funcs[fmt.Sprintf("%d", *i)]
 	if !ok {
