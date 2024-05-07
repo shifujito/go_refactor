861ffe1eea0c50ff2949c9db73a0dced6c27d16e
shifujito <fujito.shion@gmail.com>
2024-05-07 23:06:40 +0900

    77

--- ch10/main.go
+++ ch10/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 )
@@ -24,9 +25,49 @@ func no75() {
 
 func no76() {
 	// No.76 time.Afterとメモリリーク
-	// time.Afterは、
+	// time.Afterは、チェンルを返し、指定された時間が経過後にチャネルに値を送信する。
+	// time.Afterをループで使用するとメモリリークが発生する。
+	for {
+		select {
+		case <-time.After(1 * time.Second):
+			fmt.Println("tick")
+		}
+	}
+}
+
+// Json処理の間違い
+type Event struct {
+	ID        int
+	time.Time // 埋め込みフィールド
+}
+
+func no77() {
+
+	event := Event{
+		ID:   1,
+		Time: time.Now(),
+	}
+	b, err := json.Marshal(event)
+	if err != nil {
+		fmt.Println("err", err)
+		return
+	}
+	fmt.Println(string(b))
+	// time.Timeは構造体であり、MarshalJSONメソッドを持っている。
+	// そのため、time.Timeを埋め込んだ構造体をJSONに変換すると、time.Timeのフィールドが直接JSONに変換される。
+	// 解決策1. json.Marshalerを実装する。
+}
+
+func (e Event) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		ID   int
+		Time time.Time
+	}{
+		ID:   e.ID,
+		Time: e.Time,
+	})
 }
 
 func main() {
-	no75()
+	no77()
 }
